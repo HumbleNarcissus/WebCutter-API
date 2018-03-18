51337e2e7925aa20330e4088667b6e7b693f9756
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-03-18 14:58:52 +0100

    add links expired date

--- models/SiteModel.py
+++ models/SiteModel.py
@@ -1,4 +1,5 @@
 from db import db
+from datetime import datetime, timedelta
 
 class SiteModel(db.Model):
 
@@ -7,17 +8,22 @@ class SiteModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_link = db.Column(db.String, nullable=False)
     short_link = db.Column(db.String, nullable=False)
-    expired_date = db.Column(db.Date, nullable=True)
+    expired_date = db.Column(db.DateTime, nullable=True)
 
     def __init__(self, full_link, short_link):
         self.full_link = full_link
         self.short_link = short_link
+        self.expired_date = datetime.now() + timedelta(hours=1)
     
     def __repr__(self):
-        return 'full {}, short{}, expired{}'.format(self.full_link, self.short_link, self.expired_date)
+        return 'full {}, short{}, expired{}'.format(self.full_link, self.short_link, self.expired_date.strftime("%Y-%m-%d %H:%M:%S"))
     
     def json(self):
-        return {'full_link': self.full_link, 'short_link': self.short_link, 'expired': self.expired_date}
+        if isinstance(self.expired_date, datetime):
+            return {'full_link': self.full_link, 'short_link': self.short_link, 'expired': self.expired_date.strftime("%Y-%m-%d %H:%M:%S")}
+        else:
+            return {'full_link': self.full_link, 'short_link': self.short_link, 'expired': None}
+            
 
     @classmethod    
     def find_by_fullLink(cls, site_name):
