583dfb862e399fe1fcd516b150bc50a3ea2a34f4
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-04-12 23:03:28 +0200

    config file

---
+++ configs.py
@@ -0,0 +1,12 @@
+'''All config options'''
+
+import os
+
+class BaseConfig(object):
+    DEBUG = False
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
