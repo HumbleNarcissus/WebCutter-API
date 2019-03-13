8d974c3154f68ca71ff10262a6f900037f47f898
HumbleNarcissus <maciek.tarach06@gmail.com>
2019-03-13 19:34:25 +0100

    Fix code style issues suggested by codacy

--- cutter/project/tests/test_site_model.py
+++ cutter/project/tests/test_site_model.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_site, add_user
 from project.resources.SiteModel import SiteModel
-from project.resources.utils import create_shortcut
 
 
 class TestSiteModel(BaseTestCase):
@@ -29,7 +28,7 @@ class TestSiteModel(BaseTestCase):
         """
         user = add_user('mac', 'mac@mac.pl', '1234qwer')
         add_site('google.com', user_id=user.id)
-        
+
         site = SiteModel.query.filter_by(full_link="google.com").first()
 
         json_site = site.json()
@@ -46,7 +45,7 @@ class TestSiteModel(BaseTestCase):
         """
         user = add_user('mac', 'mac@mac.pl', '1234qwer')
         add_site('google.com', user_id=user.id)
-        
+
         site = SiteModel.query.filter_by(full_link="google.com").first()
         site.set_date(None)
         json_site = site.json()
@@ -56,5 +55,3 @@ class TestSiteModel(BaseTestCase):
         self.assertEqual(6, len(json_site['short_link']))
         self.assertEqual(True, json_site['working'])
         self.assertEqual(None, json_site['expiry_date'])
-
-        
